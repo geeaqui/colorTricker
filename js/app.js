a9e8a1fbbf10dfd044a076523f8b710398f8515e
Gerald Aqui <gaqui@spartaglobal.co>
2017-01-30 22:07:43 +0000

    added JQuery function fadein and fadeout in the function getColor()

--- js/app.js
+++ js/app.js
@@ -35,29 +35,37 @@ $(function(){
 			ans = "orange";
 			}else if(event.keyCode === 39){
 				ans = "red";
-				//console.log("Right");
 			}
 			compareAnswer(ans, color);
 		});
 	}
 
+	//Generate random color between orange and red
 	function getColor(){
 		var rand = Math.floor(Math.random()*colors.length);
 			if(colors[rand] === "orange"){
-				answer.style.background = "orange";
+				//answer.style.background = "orange";
+				$(answer).css('background-color','orange').fadeIn(1000, function(){
+					$(this).fadeOut(1000);
+				});
 				return "orange";
 			}else if(colors[rand] === "red"){
-				answer.style.background = "red";
+				//answer.style.background = "red";
+				$(answer).css('background-color','red').fadeIn(1000,function(){
+					$(this).fadeOut(1000);
+				});
 			return "red";
 		}
 	}
 
+	//compare answer
 	function compareAnswer(ans, color){
+		var spanScore = document.getElementById('spanScore');
 		console.log(ans,color);
 		if(ans === color){
 			console.log("You are right");
 			score +=100;
-			console.log(score);
+			spanScore.innerHTML = score;
 		}else{
 			alert("Game Over!");
 			answer.style.display = "none";
@@ -65,6 +73,7 @@ $(function(){
 
 	}
 
+	//set game speed
 	function myFunction() {
     	myVar = setInterval(listenForKeyPress, 3000);
 	}
