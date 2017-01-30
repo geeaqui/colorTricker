f6f170dee753cd5f6dcb0038e666720396c20dc6
Gerald Aqui <gaqui@spartaglobal.co>
2017-01-30 11:53:30 +0000

    Created a simple button that links to start page, game page and instruction page

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(function(){
+
+//start button
+	$('#startBtn').click(function(){
+		$('#front, #gamePage').slideToggle();
+	});
+
+	$('#back').click(function(){
+		$('#front, #gamePage').slideToggle();
+	});
+//instruction button
+	$('#intBtn').click(function(){
+		$('#front, #instrucPage').slideToggle();
+	});
+//key event listener
+});
