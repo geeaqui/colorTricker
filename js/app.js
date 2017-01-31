5586902f1590f494a7eb398c65397b12ad72fee0
Gerald Aqui <gaqui@spartaglobal.co>
2017-01-31 17:25:33 +0000

    fixed the the if else stament inside compare answer function

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ $(function(){
 	var score = 0;
 	var colorInterval;
 	var easy = 3;
-	var hard = 2;
 	var clockEnd;
 
 	//start button
@@ -43,24 +42,20 @@ $(function(){
 			if(event.keyCode === 37){
 				ans = "orange";
 				stopTimer();
-				easy = 3;
+				//easy = 3;
 			}else if(event.keyCode === 39){
 				ans = "red";
 				stopTimer();
-				easy = 3;
+				//easy = 3;
 			}
 			textAns.innerHTML = colorText[rand];
 			compareAnswer(ans, color);
 		});
-
-		// setTimeout(function() {
-		// 	$(document).off("keydown");
-		// }, 2000);
 	}
 
 	//Generate random color between orange and red
 	function getColor(){
-		start();
+		//start();
 		var rand = Math.floor(Math.random()*colors.length);
 			if(colors[rand] === "orange"){
 				$(answer).css('background-color','orange');
@@ -75,19 +70,20 @@ $(function(){
 
 	//compare answer
 	function compareAnswer(ans, color){
+		start();
 		var spanScore = document.getElementById('spanScore');
-		console.log(ans,color);
-		if(ans === color){
+		console.log(easy);
+		if(ans !== color || easy === 0 ){
+			console.log(easy);
+			console.log("Game over");
+			$(answer).css('display','none');
+			alert("game over");	
+		}else if(ans === color){
+			console.log(easy);
 			console.log("You are right");
 			score +=100;
 			spanScore.innerHTML = score;
 			listenForKeyPress();
-		}else if(easy === 0){
-			alert("Game Over!");	
-		}else{
-			console.log("Game over");
-			$(answer).css('display','none');
-			alert("game over");
 			//removeInterval();
 		}
 	}
@@ -106,46 +102,21 @@ $(function(){
   		}
 	}
 
-	//stop the game
-	function removeInterval(){
-			clearInterval(colorInterval);
-	}
-
-	//set game speed
-	function myFunction() {
-		 colorInterval = setInterval(listenForKeyPress, 3000);
-	}
-
-
 	//add set timeout if there is no answer
+	var start = function() {
+  		clockEnd = setInterval(countdown, 1000);
+	}
 
+	var countdown = function() {
+		if (easy > 0) {
+    		easy = easy - 1;
+    		console.log(easy);
+      	}
+	}
 
-
-
-
-//Testing
-
-
-var start = function() {
-  clockEnd = setInterval(countdown, 1000);
-
-  }
-
-var countdown = function() {
-	if (easy > 0) {
-    	easy = easy - 1;
-    	console.log(easy);
-    	if(easy === 0){
-    		alert("game over!");
-    	}
-      }
-}
-
-function stopTimer() {
-    clearInterval(clockEnd);
-}
-
-
+	function stopTimer() {
+    	clearInterval(clockEnd);
+	}
 
 });
 
